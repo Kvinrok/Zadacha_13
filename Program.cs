30c22e06eacc29f44df6119c85706c0682865c71
Alexadr <sanz76@.mail.ru>
2022-12-04 19:53:08 +0300

    DZ_13

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿Console.Write("Введи число: ");
+int Number = Convert.ToInt32(Console.ReadLine());
+string Text = Convert.ToString(Number);
+if (Text.Length > 2){
+  Console.WriteLine("третья цифра -> " + Text[2]);
+}
+else {
+  Console.WriteLine("-> третьей цифры нет");
+}
